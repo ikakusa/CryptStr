ce29213778b4c18ed79fce5d2a411cc7441768ad
誠 遺憾 <ikasan2860@gmail.com>
2026-08-26 10:45:33 +0900

    char以外にもwcharやchar8_tなどの他の型にも対応

--- CryptStr/CryptStr.cpp
+++ CryptStr/CryptStr.cpp
@@ -3,11 +3,22 @@
 
 int main()
 {
-	auto protected_string = protect("Hello, World!");
-	auto protected_string2 = protect("Hello, World!");
-	const char* buffer = protected_string.buffer();
-	//const char* buffer = protected_string; //operator const CharType*() is also available
+	// easy to use, just like a normal string literal
+	auto protected_hello = protect("Hello, World");
 	
+	auto protected_hello2 = protect2("Hello, World"); //Decrypt on RAII
+	const char* buffer2 = protected_hello2.buffer();
+	//const char* buffer = protected_string; //operator const CharType*() is also available
+
+	// Also works with wide strings and UTF-8 strings or any other character type
+	std::wstring protected_wide_hello = protect(L"Hello, World");
+	std::u8string protected_u8_hello = protect(u8"Hello, World");
+	//
+
 	//Output: Hello, World!
-	std::cout << buffer << std::endl;
+	std::cout << protected_hello << std::endl;
+	std::cout << buffer2 << std::endl;
+
+	std::wcout << protected_wide_hello << std::endl;
+	std::cout << reinterpret_cast<const char*>(protected_u8_hello.c_str()) << std::endl;
 }
--- CryptStr/CryptStr.h
+++ CryptStr/CryptStr.h
@@ -27,14 +27,15 @@ namespace ikakusa {
                 std::remove_cvref_t<DataType>
                 >
                 >;
-            using Type = CharType[Len - 1];
+            using Type = CharType[Len / sizeof(CharType) - 1];
         private:
             Type _buffer{};
         public:
-            scoped_string(protected_string* _instance, std::uint32_t* d, size_t count, size_t len) {
-                for (size_t i = 0; i < len - 1; ++i) {
-                    _buffer[i] = d[i] ^ _instance->generate_seed(i, count);
+            scoped_string(protected_string* _instance, CharType* d, size_t count, size_t len) {
+                for (size_t i = 0; i < len / sizeof(CharType) - 1; ++i) {
+                    _buffer[i] = static_cast<CharType>(static_cast<uint32_t>(d[i]) ^ _instance->generate_seed(i, count));
                 }
+                _buffer[len / sizeof(CharType) - 1] = CharType{};
             }
             ~scoped_string() {
 				SecureZeroMemory(_buffer, sizeof(_buffer));
@@ -49,7 +50,7 @@ namespace ikakusa {
     public:
         size_t len;
         size_t count;
-        std::uint32_t data[Len - 1];
+        CharType data[Len / sizeof(CharType) - 1];
         FORCEINLINE consteval size_t size() {
             return Len - 1;
         }
@@ -72,11 +73,21 @@ namespace ikakusa {
         consteval explicit protected_string(DataType _data) {
             count = Count;
             len = Len;
-            for (size_t i = 0; i < len - 1; ++i) {
-                data[i] = _data[i] ^ generate_seed(i, count);
+            for (size_t i = 0; i < len / sizeof(CharType) - 1; ++i) {
+                data[i] = static_cast<CharType>(static_cast<uint32_t>(_data[i]) ^ generate_seed(i, count));
             }
         }
         NOINLINE auto reveal() {
+            using Type = std::basic_string<CharType>;
+            Type result{};
+			result.resize(len - 1);
+            for (size_t i = 0; i < len / sizeof(CharType) - 1; ++i) {
+                result[i] = static_cast<CharType>(static_cast<uint32_t>(data[i]) ^ generate_seed(i, count));
+            }
+			result[len / sizeof(CharType) - 1] = CharType{};
+            return result;
+        }
+        NOINLINE auto reveal2() {
             auto _result = scoped_string(this, data, count, len);
 			return _result;
         }
@@ -84,4 +95,7 @@ namespace ikakusa {
 }
 
 #define protect(str) \
-ikakusa::protected_string<decltype(str), sizeof(str), __COUNTER__>(str).reveal()
+ikakusa::protected_string<decltype(str)&, sizeof(str), __COUNTER__>(str).reveal()
+
+#define protect2(str) \
+ikakusa::protected_string<decltype(str)&, sizeof(str), __COUNTER__>(str).reveal2()
