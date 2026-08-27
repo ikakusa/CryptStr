d7f8ec49fc6cc1e0a1ce6d514d83314cfed948ac
誠 遺憾 <ikasan2860@gmail.com>
2026-08-27 23:07:40 +0900

    サンプルのコメント更新

--- CryptStr/CryptStr.cpp
+++ CryptStr/CryptStr.cpp
@@ -8,10 +8,10 @@ int main()
 	
 	// Also works with wide strings and UTF-8 strings or any other character type
 	std::wstring protected_wide_hello = protect(L"Hello, World");
-	std::u8string protected_u8_hello = ikakusa::protected_string<decltype(u8"Hello, World")&, sizeof(u8"Hello, World"), 14>(u8"Hello, World", std::make_index_sequence<(sizeof(u8"Hello, World") / sizeof(std::remove_cv_t<std::remove_extent_t<std::remove_cvref_t<decltype(u8"Hello, World")>>>) - 1)>{}).reveal(std::make_index_sequence<(sizeof(u8"Hello, World") / sizeof(std::remove_cv_t<std::remove_extent_t<std::remove_cvref_t<decltype(u8"Hello, World")>>>) - 1)>{});
+	std::u8string protected_u8_hello = protect(u8"Hello, World");
 	//
 
-	// Also there is a lite weighted version of the protection, which is faster and smaller, but less secure
+	// Also there is a lite weighted version of the protection, which is smaller
 	std::string lite_protected_hello = lprotect("Hello, World");
 	//
 
